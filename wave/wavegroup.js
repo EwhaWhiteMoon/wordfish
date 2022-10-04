4d0aafb68795c6b4330f39045b46d3cd37003f89
seyoung Kim <suyasuyazzang@gmail.com>
2022-10-04 15:57:08 +0900

    Wave 색/위치로 바다를 표현

--- wave/wavegroup.js
+++ wave/wavegroup.js
@@ -6,10 +6,12 @@ export class WaveGroup{
     constructor() {
         this.waves = [];
 
-        this.waves[0] = new Wave(4, 0, 'rgba(230, 230, 255, 1)', 0.05, 0.025, 10);
-        this.waves[1] = new Wave(7, 0, 'rgba(160, 200, 255, 0.2)', 0.2, 0.05, 20);
-        this.waves[2] = new Wave(7, 1, 'rgba(160, 200, 255, 0.2)', 0.2, 0.025, 20);
-        this.waves[3] = new Wave(4, 1, 'rgba(225, 210, 210, 1)', 0.9, 0.001, 40);
+        this.waves[0] = new Wave(4, 0, 'rgba(235, 245, 255, 1)', 0.05, 0.025, 10);
+        this.waves[1] = new Wave(7, 0, 'rgba(175, 220, 255, 0.3)', 0.2, 0.05, 20);
+        this.waves[2] = new Wave(7, 1, 'rgba(175, 220, 255, 0.3)', 0.2, 0.025, 20);
+        this.waves[3] = new Wave(7, 1, 'rgba(175, 210, 255, 0.4)', 0.6, 0.03, 10);
+        this.waves[4] = new Wave(7, 1, 'rgba(175, 210, 255, 0.4)', 0.7, 0.04, 10);
+        this.waves[5] = new Wave(4, 1, 'rgba(220, 190, 170, 1)', 0.9, 0.001, 40);
     }
 
     resize(stageWidth, stageHeight){
